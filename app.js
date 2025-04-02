b1f6d4056ff4beec9916afd6c574fa0a00d358c3
Haniya Fatima <66930662+haniyafatima@users.noreply.github.com>
2025-04-01 23:43:06 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+
+document.getElementById("favs").addEventListener("click", main);
+
+function main(){
+    document.getElementById("favs").style.color = "red";
+}
+  
+
+
+
+
+
+// // var collection = document.getElementsByClassName("hrt");
+// // collection.addEventListener("click", main);
+// // i = collection.length;
+// // function main(){
+// //     i = collection.length;
+
+// //     for (let i = 0; i <= collection.length; i++) {
+// //         collection[i].style.color = "red";
+// //       }
+// // }
+// i = collection.lenght;
+
+// var collection = document.getElementsByClassName("hrt")[i];
+// collection.addEventListener("click", main);
+
+// function main(){
+//     i = collection.lenght;
+//     for(let i = 0; i <= collection.lenght; i++)
+//     {
+// collection[i].style.color = "red";
+//     }
+// }
+
